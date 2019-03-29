9814de04ac018744e93a20c89869394c888cb5af
David Sanchez <david.sanchez@nventive.com>
2019-03-29 10:54:08 -0400

    Updating code using extensions metadata

--- DependencyInjection/Compiler/ExtensionCompilerPass.php
+++ DependencyInjection/Compiler/ExtensionCompilerPass.php
@@ -2,10 +2,15 @@
 
 namespace Harmony\Bundle\ExtensionBundle\DependencyInjection\Compiler;
 
-use Harmony\Sdk\Extension\ExtensionInterface;
+use SplFileInfo;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Finder\Finder;
+use function array_merge_recursive;
+use function explode;
+use function is_dir;
+use function preg_match;
+use function substr_count;
 
 /**
  * Class ExtensionCompilerPass
@@ -26,28 +31,26 @@ class ExtensionCompilerPass implements CompilerPassInterface
         $translatorDefinition           = $container->findDefinition('translator.default');
 
         $transPaths = [];
-        foreach ($container->getParameter('kernel.extensions') as $namespace => $class) {
-            /** @var ExtensionInterface $extensionClass */
-            $extensionClass = new $class();
-
+        foreach ($container->getParameter('kernel.extensions_metadata') as $name => $extension) {
             // register extensions as Twig namespaces
-            if (\is_dir($path = $extensionClass->getPath() . '/Resources/views')) {
-                $twigFilesystemLoaderDefinition->addMethodCall('addPath', [$path, $namespace]);
+            if (is_dir($path = $extension['path'] . '/Resources/views')) {
+                $twigFilesystemLoaderDefinition->addMethodCall('addPath', [$path, $extension['namespace']]);
             }
             // set translations directory path if exists
-            if (\is_dir($dir = $extensionClass->getPath() . '/Resources/translations')) {
+            if (is_dir($dir = $extension['path'] . '/Resources/translations')) {
                 $transPaths[] = $dir;
             }
         }
 
         // Register translation resources
         if ($transPaths) {
-            $files = [];
+            $files  = [];
             $finder = Finder::create()
                 ->followLinks()
                 ->files()
-                ->filter(function (\SplFileInfo $file) {
-                    return 2 === substr_count($file->getBasename(), '.') && preg_match('/\.\w+$/', $file->getBasename());
+                ->filter(function (SplFileInfo $file) {
+                    return 2 === substr_count($file->getBasename(), '.') &&
+                        preg_match('/\.\w+$/', $file->getBasename());
                 })
                 ->in($transPaths)
                 ->sortByName();
